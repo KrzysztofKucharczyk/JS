1903ef0942015d820c65ad8d615efc256773085f
Krzysztof Kucharczyk <krzysztof.kucharczyk@capgemini.com>
2016-04-19 10:53:46 +0200

    Fixing not required lines.

--- js/router.js
+++ js/router.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 var router = (function() {
 
     var homePage = document.getElementById('homePage'),
@@ -26,5 +25,3 @@ var router = (function() {
     };
 
 })();
-
-//router.showHomePage();
